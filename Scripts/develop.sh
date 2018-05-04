a55bb734203e73274057b82ea4e0d16dbf0d8102
CoderAFI <developer_afi@163.com>
2018-05-04 12:27:23 +0800

    modify transmit.

--- Scripts/develop.sh
+++ Scripts/develop.sh
@@ -151,8 +151,8 @@ docker ps
 
 # Cask install macOS apps
 brew cask install google-chrome dropbox foxmail
-brew cask install wwdc cheatsheet sourcetree telegram slack insomnia postman gitup asset-catalog-tinkerer transmit
-brew cask squirrel hyper spark sitesucker # optional
+brew cask install wwdc cheatsheet sourcetree telegram slack insomnia postman gitup asset-catalog-tinkerer
+brew cask squirrel hyper spark sitesucker transmit # optional
 
 # Intellij Platform
 brew cask install jetbrains-toolbox
