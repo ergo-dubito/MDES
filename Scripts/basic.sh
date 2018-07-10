47ec48bce8bdaa85896539b98c04bd85d54180e1
CoderAFI <developer_afi@163.com>
2018-07-10 13:20:12 +0800

    update font resource.

--- Scripts/basic.sh
+++ Scripts/basic.sh
@@ -5,19 +5,16 @@ ssh-add -K ~/.ssh/id_rsa_github
 chmod 600 ~/.ssh/id_rsa_github
 
 # Font
-brew install cabextract
-cd ~/Downloads
-mkdir consolas
-cd consolas
-curl -O http://download.microsoft.com/download/f/5/a/f5a3df76-d856-4a61-a6bd-722f52a5be26/PowerPointViewer.exe
-cabextract PowerPointViewer.exe
-cabextract ppviewer.cab
-open CONSOLA*.TTF
 
+# Consolas
+http://www.fontpalace.com/font-download/Consolas/
+
+# FiraCode
 git clone git@github.com:tonsky/FiraCode.git
 cd FiraCode/distr/otf
 open FiraCode-*.otf
 
+# OfficeCodePro
 git clone git@github.com:nathco/Office-Code-Pro.git
 cd Office-Code-Pro/Fonts/Office\ Code\ Pro/OTF/
 open OfficeCodePro-*.otf
